0e58093f9c112c61163fa45ced121832f952df0c
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-30 19:37:44 -0500

    Setup basic php page with variables

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//get the data from a GET request
+$first_name = $_GET['first_name'];
+$last_name = $_GET['last_name'];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Name Test</title>
+        <link rel="stylesheet" href="main.css" type="text/css">
+    </head>
+    <body>
+        <h2>Welcome</h2>
+        <p>First Name: <?php echo $first_name; ?></p>
+        <p>Last Name: <?php echo $last_name; ?></p>
+    </body>
+</html>
